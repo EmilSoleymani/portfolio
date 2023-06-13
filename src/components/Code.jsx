a4e296c9c13b0790e65097345b412d16abc9f440
Emil Soleymani <esoleymani@venacorp.com>
2023-06-13 19:06:52 -0400

    Added more languages and better styling

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -1,11 +1,17 @@
 import { useContext, useEffect, useState } from 'react';
 import CopyToClipboard from 'react-copy-to-clipboard';
-import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { materialDark, materialLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import { ThemeContext } from '../App.js';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClone, faCheck } from '@fortawesome/free-solid-svg-icons';
+
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { materialDark, materialLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { ThemeContext } from '../App.js';
+import python from 'react-syntax-highlighter/dist/esm/languages/prism/python';
+import java from 'react-syntax-highlighter/dist/esm/languages/prism/java';
+
+SyntaxHighlighter.registerLanguage('python', python);
+SyntaxHighlighter.registerLanguage('java', java);
 
 const Code = ({ children, language }) => {
   const [copied, setCopied] = useState(false);
@@ -26,7 +32,7 @@ const Code = ({ children, language }) => {
         </button>
       </CopyToClipboard>
       <SyntaxHighlighter
-        language="java"
+        language={language}
         style={isDark ? materialDark : materialLight}
       >
         {children}
--- src/markdown/article.md
+++ src/markdown/article.md
@@ -2,9 +2,29 @@
 
 * Markdown
 
+In line `code`
+
 Hello World, auto deployed from GitHub Actions!
+
+> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+
+* Lists
+* [ ] todo
+* [x] done
 
 <Code language="python">
 def main():
     print("Hello")
+</Code>
+
+<Code language="java">
+public static void main(String[] args){
+    ArrayList\<Integer\> ints;
+}
+</Code>
+
+<Code language="javascript">
+const helloWorld = () => {
+    console.log("Hello World")
+}
 </Code>
