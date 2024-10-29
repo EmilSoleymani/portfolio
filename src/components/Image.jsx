b83256936c5df2e2d70d66df42087a2d846761ad
Emil Soleymani <soleymae@mcmaster.ca>
2024-10-28 21:42:20 -0400

    Add custom image prop

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Image = ({src, alt, styles}) => {
+const Image = ({src, alt, style}) => {
   return (
     // STYLE prop instead of style because that allows me to pass in CSS as string
-    <img src={src} alt={alt} STYLE={styles}/>
+    <img src={src} alt={alt} style={style}/>
   )
 }
 
