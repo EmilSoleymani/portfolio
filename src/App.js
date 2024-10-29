28f8b6366f3a0e3b822cbf874f33d76697bcebf4
Emil Soleymani <soleymae@mcmaster.ca>
2024-10-29 11:29:23 -0400

    Use symlink of portfolio-markdown for local testing:

--- src/App.js
+++ src/App.js
@@ -5,18 +5,32 @@ import Post from './components/Post';
 
 export const ThemeContext = createContext();
 
-const endpoint = 'https://emil-soleymani-portfolio-markdown.s3.amazonaws.com/articles.json'
+const LOCAL = true;
+const S3_BUCKET_URL = 'https://emil-soleymani-portfolio-markdown.s3.amazonaws.com/'
+const LOCAL_URL = './portfolio-markdown/'
 
 function App() {
 
   const [blogData, setBlogData] = useState([])
 
   useEffect(() => {
-    fetch(endpoint)
+    const endpoint = LOCAL ? LOCAL_URL : S3_BUCKET_URL
+    fetch(endpoint + 'articles.json')
       .then(response => response.json())
       .then(data => setBlogData(data.blog))
       .catch(error => console.log(error));    
   }, []);
+
+  function getBlogUrl(url) {
+    if (LOCAL) {
+      // Remove the default S3_BUCKET prefix if LOCAL
+      let n = S3_BUCKET_URL.length;
+      url = url.slice(n);
+      // Add LOCAL_URL to beginning instead
+      url = LOCAL_URL + url
+    }
+    return url
+  }
 
   return (
     <div>
@@ -25,7 +39,7 @@ function App() {
       <div className="body-wrapper">
         {
           blogData.sort((a, b) => new Date(b.date) - new Date(a.date)).map((blog, key) => (
-            <Post key={key} contentPath={blog.url} title={blog.title} date={blog.date}></Post>
+            <Post key={key} contentPath={getBlogUrl(blog.url)} title={blog.title} date={blog.date}></Post>
           ))
         }
       </div>
